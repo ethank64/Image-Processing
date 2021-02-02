c6bea67c8885cd468b3960ad15afea06e4208055
ethan-k64 <59579977+ethan-k64@users.noreply.github.com>
2021-02-02 12:11:03 -0500

    Added AfterImage class file.

---
+++ AfterImage.js
@@ -0,0 +1,109 @@
+class AfterImage {
+  constructor(pixels, x, y) {
+    this.pixels = pixels;
+    this.pos = createVector(x, y);
+  }
+
+  render() {
+    for (let i = 0; i < width / 2; i++) {
+      for (let j = 0; j < height; j++) {
+        strokeWeight(1);
+        stroke(this.pixels[i][j][0], this.pixels[i][j][1], this.pixels[i][j][2], this.pixels[i][j][3])
+        point(i + this.pos.x, j + this.pos.y);
+      }
+    }
+  }
+
+  fillArray() {
+    for (let i = 0; i < width / 2; i++) {
+      for (let j = 0; j < height; j++) {
+        this.pixels[i][j] = canvas.getContext('2d').getImageData(i, j, 1, 1).data;
+      }
+    }
+  }
+
+  pixelate(res) {
+    let avg = [0, 0, 0];
+
+    for (let xoff = 0; xoff < width / 2 / res - 1; xoff++) {
+      for (let yoff = 0; yoff < height / res - 1; yoff++) {
+        for (let i = res * xoff; i < res + res * xoff; i++) {
+          for (let j = res * yoff; j < res + res * yoff; j++) {
+            avg[0] += this.pixels[i][j][0];   
+            avg[1] += this.pixels[i][j][1];
+            avg[2] += this.pixels[i][j][2];
+          }
+        }
+
+        avg[0] /= (res * res);
+        avg[1] /= (res * res);
+        avg[2] /= (res * res);
+
+        for (let i = res * xoff; i < res + res * xoff; i++) {
+          for (let j = res * yoff; j < res + res * yoff; j++) {
+            this.pixels[i][j][0] = avg[0];
+            this.pixels[i][j][1] = avg[1];
+            this.pixels[i][j][2] = avg[2];
+          }
+        }
+
+        avg[0] = 0;
+        avg[1] = 0;
+        avg[2] = 0;
+      }
+    }
+  }
+
+  blackAndWhite(inverse) {
+    for (let i = 0; i < width / 2; i++) {
+      for (let j = 0; j < height; j++) {
+        if ((this.pixels[i][j][0] + this.pixels[i][j][1] + this.pixels[i][j][2]) < 383 && inverse === false) {
+          this.pixels[i][j][0] = 0;
+          this.pixels[i][j][1] = 0;
+          this.pixels[i][j][2] = 0;
+        } else if ((this.pixels[i][j][0] + this.pixels[i][j][1] + this.pixels[i][j][2]) >= 383 && inverse === false) {
+          this.pixels[i][j][0] = 255;
+          this.pixels[i][j][1] = 255;
+          this.pixels[i][j][2] = 255;
+        }
+
+        if ((this.pixels[i][j][0] + this.pixels[i][j][1] + this.pixels[i][j][2]) < 383 && inverse === true) {
+          this.pixels[i][j][0] = 255;
+          this.pixels[i][j][1] = 255;
+          this.pixels[i][j][2] = 255;
+        } else if ((this.pixels[i][j][0] + this.pixels[i][j][1] + this.pixels[i][j][2]) >= 383 && inverse === true) {
+          this.pixels[i][j][0] = 0;
+          this.pixels[i][j][1] = 0;
+          this.pixels[i][j][2] = 0;
+        }
+      }
+    }
+  }
+
+  makeCutLines() {
+    let count = 0;
+
+    for (let i = 0; i < width / 2; i++) {
+      for (let j = 0; j < height; j++) {
+        if (this.pixels[i][j][0] === 0) {
+          for (let xoff = -1; xoff <= 1; xoff++) {
+            for (let yoff = -1; yoff <= 1; yoff++) {
+              let check = createVector(i + xoff, j + yoff);
+              if (check.x > -1 && check.x < width / 2 && check.y > -1 && check.y < height) {
+                if (this.pixels[i + xoff][j + yoff][0] === 255 && this.pixels[i + xoff][j + yoff][1] === 255 && this.pixels[i + xoff][j + yoff][2] === 255) {
+                  count++;
+                }
+              }
+            }
+          }
+        }
+
+        if (count > 0) {
+          this.pixels[i][j][0] = 255;
+        }
+
+        count = 0;
+      }
+    }
+  }
+}
