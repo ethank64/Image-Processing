f9602e4c132d6e9246dc854362fb1e5b4646943e
ethan-k64 <59579977+ethan-k64@users.noreply.github.com>
2021-02-12 23:14:34 -0500

    Pixelate function now pixelates the whole image.

--- AfterImage.js
+++ AfterImage.js
@@ -1,12 +1,14 @@
 class AfterImage {
-  constructor(pixels, x, y) {
+  constructor(pixels, x, y, w, h) {
     this.pixels = pixels;
     this.pos = createVector(x, y);
+    this.w = w;
+    this.h = h;
   }
 
   render() {
-    for (let i = 0; i < width / 2; i++) {
-      for (let j = 0; j < height; j++) {
+    for (let i = 0; i < this.w; i++) {
+      for (let j = 0; j < this.h; j++) {
         strokeWeight(1);
         stroke(this.pixels[i][j][0], this.pixels[i][j][1], this.pixels[i][j][2], this.pixels[i][j][3])
         point(i + this.pos.x, j + this.pos.y);
@@ -15,8 +17,8 @@ class AfterImage {
   }
 
   fillArray() {
-    for (let i = 0; i < width / 2; i++) {
-      for (let j = 0; j < height; j++) {
+    for (let i = 0; i < this.w; i++) {
+      for (let j = 0; j < this.h; j++) {
         this.pixels[i][j] = canvas.getContext('2d').getImageData(i, j, 1, 1).data;
       }
     }
@@ -24,26 +26,45 @@ class AfterImage {
 
   pixelate(res) {
     let avg = [0, 0, 0];
+    let last = false;
+    let remainder = createVector(this.w % res, this.h % res);
 
-    for (let xoff = 0; xoff < width / 2 / res - 1; xoff++) {
-      for (let yoff = 0; yoff < height / res - 1; yoff++) {
+    for (let xoff = 0; xoff < this.w / res; xoff++) {
+      for (let yoff = 0; yoff < this.h / res; yoff++) {
         for (let i = res * xoff; i < res + res * xoff; i++) {
           for (let j = res * yoff; j < res + res * yoff; j++) {
-            avg[0] += this.pixels[i][j][0];   
-            avg[1] += this.pixels[i][j][1];
-            avg[2] += this.pixels[i][j][2];
+            if (j < this.h && i < this.w) {
+              avg[0] += this.pixels[i][j][0];
+              avg[1] += this.pixels[i][j][1];
+              avg[2] += this.pixels[i][j][2];
+              last = false;
+            } else {
+              last = true;
+              break;
+            }
           }
         }
 
-        avg[0] /= (res * res);
-        avg[1] /= (res * res);
-        avg[2] /= (res * res);
+        if (last === true) {
+          avg[0] /= (remainder.x * remainder.y);
+          avg[1] /= (remainder.x * remainder.y);
+          avg[2] /= (remainder.x * remainder.y);
+        } else {
+          avg[0] /= (res * res);
+          avg[1] /= (res * res);
+          avg[2] /= (res * res);
+        }
+
 
         for (let i = res * xoff; i < res + res * xoff; i++) {
           for (let j = res * yoff; j < res + res * yoff; j++) {
-            this.pixels[i][j][0] = avg[0];
-            this.pixels[i][j][1] = avg[1];
-            this.pixels[i][j][2] = avg[2];
+            if (j < this.h && i < this.w) {
+              this.pixels[i][j][0] = avg[0];
+              this.pixels[i][j][1] = avg[1];
+              this.pixels[i][j][2] = avg[2];
+            } else {
+              break;
+            }
           }
         }
 
@@ -55,8 +76,8 @@ class AfterImage {
   }
 
   blackAndWhite(inverse) {
-    for (let i = 0; i < width / 2; i++) {
-      for (let j = 0; j < height; j++) {
+    for (let i = 0; i < this.w; i++) {
+      for (let j = 0; j < this.h; j++) {
         if ((this.pixels[i][j][0] + this.pixels[i][j][1] + this.pixels[i][j][2]) < 383 && inverse === false) {
           this.pixels[i][j][0] = 0;
           this.pixels[i][j][1] = 0;
@@ -83,13 +104,13 @@ class AfterImage {
   makeCutLines() {
     let count = 0;
 
-    for (let i = 0; i < width / 2; i++) {
-      for (let j = 0; j < height; j++) {
+    for (let i = 0; i < this.w; i++) {
+      for (let j = 0; j < this.h; j++) {
         if (this.pixels[i][j][0] === 0) {
           for (let xoff = -1; xoff <= 1; xoff++) {
             for (let yoff = -1; yoff <= 1; yoff++) {
               let check = createVector(i + xoff, j + yoff);
-              if (check.x > -1 && check.x < width / 2 && check.y > -1 && check.y < height) {
+              if (check.x > -1 && check.x < this.w && check.y > -1 && check.y < this.h) {
                 if (this.pixels[i + xoff][j + yoff][0] === 255 && this.pixels[i + xoff][j + yoff][1] === 255 && this.pixels[i + xoff][j + yoff][2] === 255) {
                   count++;
                 }
