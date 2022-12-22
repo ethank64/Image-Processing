11e09837b2ed0cf1350607486fdbb618401e7f11
ethank64 <64279690+ethank64@users.noreply.github.com>
2022-12-21 23:12:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,18 +10,32 @@ function preload() {
 function setup() {  
   const cnv = createCanvas(window.innerWidth, window.innerHeight);
   
+  pixelDensity(1);
+  
   image(img, 0, 0);
   
-  let ppp = floor(img.width / 8);
+  let ppp = floor(img.width / 32);
   
   colorArray = make2dArray(img.width, img.height);
   afterImage = new AfterImage(colorArray, img.width, 0, img.width, img.height);
   afterImage.copyImage();
+  //afterImage.roundColors(2);
+  afterImage.invertColors();
+  //afterImage.flip();
+  //afterImage.scramble(10000);
+  //afterImage.rainbowStatic(0, 100);
+  //afterImage.onlyMainColors(70);
+  //afterImage.grayScale();
+  //afterImage.pixelate(ppp);
   //afterImage.blackAndWhite(false);
   //afterImage.makeCutLines();
-  afterImage.pixelate(ppp);
   
   afterImage.render();
+}
+
+function draw() {
+  //afterImage.scramble(500);
+  //afterImage.render();
 }
 
 const make2dArray = (rows, cols) => {
